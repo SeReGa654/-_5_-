546571604e5d870a062689af0f5c30ab13b35afb
SERHII SOLONINKO <102055965+SeReGa654@users.noreply.github.com>
2024-04-03 01:34:43 +0300

    lab5

---
+++ AddressForm_lab/AddressForm_lab/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿using System.Windows.Forms;
+
+namespace AddressForm_lab
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.regionComboBox = new System.Windows.Forms.ComboBox();
+            this.cityComboBox = new System.Windows.Forms.ComboBox();
+            this.postalCodeTextBox = new System.Windows.Forms.TextBox();
+            this.streetAndNumberTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // regionComboBox
+            // 
+            this.regionComboBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.regionComboBox.Location = new System.Drawing.Point(201, 12);
+            this.regionComboBox.Name = "regionComboBox";
+            this.regionComboBox.Size = new System.Drawing.Size(238, 30);
+            this.regionComboBox.TabIndex = 0;
+            regionComboBox.SelectedIndexChanged += regionComboBox_SelectedIndexChanged;
+            // cityComboBox
+            // 
+            this.cityComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cityComboBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cityComboBox.Location = new System.Drawing.Point(201, 48);
+            this.cityComboBox.Name = "cityComboBox";
+            this.cityComboBox.Size = new System.Drawing.Size(238, 30);
+            this.cityComboBox.TabIndex = 1;
+            // 
+            // postalCodeTextBox
+            // 
+            this.postalCodeTextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.postalCodeTextBox.Location = new System.Drawing.Point(201, 84);
+            this.postalCodeTextBox.Name = "postalCodeTextBox";
+            this.postalCodeTextBox.Size = new System.Drawing.Size(238, 29);
+            this.postalCodeTextBox.TabIndex = 2;
+            // 
+            // streetAndNumberTextBox
+            // 
+            this.streetAndNumberTextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.streetAndNumberTextBox.Location = new System.Drawing.Point(201, 119);
+            this.streetAndNumberTextBox.Name = "streetAndNumberTextBox";
+            this.streetAndNumberTextBox.Size = new System.Drawing.Size(238, 29);
+            this.streetAndNumberTextBox.TabIndex = 3;
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.saveButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveButton.Location = new System.Drawing.Point(230, 154);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(163, 41);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Зберегти";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(43, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Регіон (область)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(121, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Місто";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(35, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Поштовий індекс";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(3, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Вулиця та №будинку";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SpringGreen;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(7, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 31);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "у файл";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(462, 207);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.streetAndNumberTextBox);
+            this.Controls.Add(this.postalCodeTextBox);
+            this.Controls.Add(this.cityComboBox);
+            this.Controls.Add(this.regionComboBox);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "MainForm";
+            this.Text = "DELIVERY";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox regionComboBox;
+        private System.Windows.Forms.ComboBox cityComboBox;
+        private System.Windows.Forms.TextBox postalCodeTextBox;
+        private System.Windows.Forms.TextBox streetAndNumberTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+    }
+
+}
+
---
+++ AddressForm_lab/AddressForm_lab/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AddressForm_lab
+{
+    public partial class MainForm : Form
+    {
+        private string[] kyivCities = { "Київ", "Бровари", "Буча", "Васильків", "Вишневе", "Ірпінь", "Обухів", "Фастів", "Боярка", "Вишгород" };
+        private string[] vinnytsiaCities = { "Вінниця", "Хмільник", "Могилів-Подільський", "Козятин", "Кропивницький", "Жмеринка", "Калинівка", "Ладижин", "Тульчин", "Гайсин" };
+        private string[] zhytomyrCities = { "Житомир", "Бердичів", "Новоград-Волинський", "Коростень", "Малин", "Народичі", "Овруч", "Радомишль", "Чуднів", "Брусилів" };
+        private string[] khmelnytskyiCities = { "Хмельницький", "Кам'янець-Подільський", "Ізяслав", "Старокостянтинів", "Шепетівка", "Волочиськ", "Славута", "Деражня", "Нетішин", "Дунаївці" };
+        private string[] cherkasyCities = { "Черкаси", "Умань", "Звенигородка", "Корсунь-Шевченківський", "Сміла", "Канів", "Жашків", "Христинівка", "Катеринопіль", "Монастирище" };
+        private string[] rivneCities = { "Рівне", "Дубно", "Острог", "Сарни", "Здолбунів", "Костопіль", "Радивилів", "Березне", "Квасилів", "Володимирець" };
+        private string[] ternopilCities = { "Тернопіль", "Кременець", "Чортків", "Тернопіль", "Бережани", "Підволочиськ", "Збараж", "Монастириська", "Ланівці", "Бучач" };
+        private string[] ivanoFrankivskCities = { "Івано-Франківськ", "Коломия", "Надвірна", "Калуш", "Яремче", "Богородчани", "Долина", "Тисмениця", "Снятин", "Городенка" };
+        private string[] chernivtsiCities = { "Чернівці", "Новодністровськ", "Сокиряни", "Хотин", "Вижниця", "Сторожинець", "Глибока", "Кельменці", "Заставна", "Герца" };
+        private string[] volynCities = { "Луцьк", "Ковель", "Володимир-Волинський", "Нововолинськ", "Кременчук", "Іваничі", "Камінь-Каширський", "Любомль", "Рожище", "Ківерці" };
+        private string[] zakarpattiaCities = { "Ужгород", "Мукачево", "Хуст", "Берегове", "Свалява", "Тячів", "Виноградів", "Іршава", "Рахів", "Великий Березний" };
+        private string[] odesaCities = { "Одеса", "Білгород-Дністровський", "Ізмаїл", "Южне", "Чорноморськ", "Подільськ", "Ананьїв", "Балта", "Роздільна", "Котовськ" };
+        private string[] mykolaivCities = { "Миколаїв", "Первомайськ", "Южноукраїнськ", "Вознесенськ", "Очаків", "Новий Буг", "Баштанка", "Снігурівка", "Березнегувате", "Доманівка" };
+        private string[] khersonCities = { "Херсон", "Нова Каховка", "Каховка", "Таврійськ", "Олешки", "Берислав", "Гола Пристань", "Скадовськ", "Генічеськ", "Чаплинка" };
+        private string[] dnipropetrovskCities = { "Дніпро", "Кривий Ріг", "Кам'янське", "Нікополь", "Павлоград", "Дніпродзержинськ", "Новомосковськ", "Верхньодніпровськ", "Синельникове", "Марганець" };
+        private string[] zaporizhiaCities = { "Запоріжжя", "Мелітополь", "Бердянськ", "Енергодар", "Токмак", "Пологи", "Гуляйполе", "Оріхів", "Василівка", "Дніпрорудне" };
+        private string[] kirovohradCities = { "Кропивницький", "Олександрія", "Світловодськ", "Знам'янка", "Новоукраїнка", "Мала Виска", "Долинська", "Бобринець", "Гайворон", "Онуфріївка" };
+        private string[] poltavaCities = { "Полтава", "Кременчук", "Миргород", "Лубни", "Горішні Плавні", "Гадяч", "Карлівка", "Пирятин", "Лохвиця", "Зіньків" };
+        private string[] sumyCities = { "Суми", "Конотоп", "Шостка", "Ромни", "Охтирка", "Лебедин", "Глухів", "Путивль", "Кролевець", "Білопілля" };
+        private string[] kharkivCities = { "Харків", "Лозова", "Ізюм", "Чугуїв", "Первомайський", "Красноград", "Богодухів", "Валки", "Дергачі", "Зміїв" };
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeRegionDropdown();
+        }
+
+        private void InitializeRegionDropdown()
+        {
+            regionComboBox.Items.AddRange(new string[] { "Київська", "Вінницька", "Житомирська", "Хмельницька", "Черкаська", "Рівненська", "Тернопільська", "Івано-Франківська", "Чернівецька", "Волинська", "Закарпатська", "Одеська", "Миколаївська", "Херсонська", "Дніпропетровська", "Запорізька", "Кіровоградська", "Полтавська", "Сумська", "Харківська" });
+
+        }
+
+        private void regionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cityComboBox.Items.Clear();
+            string selectedRegion = regionComboBox.SelectedItem.ToString();
+
+            switch (selectedRegion)
+            {
+                case "Київська":
+                    cityComboBox.Items.AddRange(kyivCities);
+                    break;
+                case "Вінницька":
+                    cityComboBox.Items.AddRange(vinnytsiaCities);
+                    break;
+                case "Житомирська":
+                    cityComboBox.Items.AddRange(zhytomyrCities);
+                    break;
+                case "Хмельницька":
+                    cityComboBox.Items.AddRange(khmelnytskyiCities);
+                    break;
+                case "Черкаська":
+                    cityComboBox.Items.AddRange(cherkasyCities);
+                    break;
+                case "Рівненська":
+                    cityComboBox.Items.AddRange(rivneCities);
+                    break;
+                case "Тернопільська":
+                    cityComboBox.Items.AddRange(ternopilCities);
+                    break;
+                case "Івано-Франківська":
+                    cityComboBox.Items.AddRange(ivanoFrankivskCities);
+                    break;
+                case "Чернівецька":
+                    cityComboBox.Items.AddRange(chernivtsiCities);
+                    break;
+                case "Волинська":
+                    cityComboBox.Items.AddRange(volynCities);
+                    break;
+                case "Закарпатська":
+                    cityComboBox.Items.AddRange(zakarpattiaCities);
+                    break;
+                case "Одеська":
+                    cityComboBox.Items.AddRange(odesaCities);
+                    break;
+                case "Миколаївська":
+                    cityComboBox.Items.AddRange(mykolaivCities);
+                    break;
+                case "Херсонська":
+                    cityComboBox.Items.AddRange(khersonCities);
+                    break;
+                case "Дніпропетровська":
+                    cityComboBox.Items.AddRange(dnipropetrovskCities);
+                    break;
+                case "Запорізька":
+                    cityComboBox.Items.AddRange(zaporizhiaCities);
+                    break;
+                case "Кіровоградська":
+                    cityComboBox.Items.AddRange(kirovohradCities);
+                    break;
+                case "Полтавська":
+                    cityComboBox.Items.AddRange(poltavaCities);
+                    break;
+                case "Сумська":
+                    cityComboBox.Items.AddRange(sumyCities);
+                    break;
+                case "Харківська":
+                    cityComboBox.Items.AddRange(kharkivCities);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        //regionComboBox.SelectedIndexChanged += regionComboBox_SelectedIndexChanged;
+        private void saveButton_Click(object sender, EventArgs e)
+{
+            string region = regionComboBox.SelectedItem?.ToString();
+            string city = cityComboBox.SelectedItem?.ToString();
+            string postalCode = postalCodeTextBox.Text;
+            string streetAndNumber = streetAndNumberTextBox.Text;
+
+            if (string.IsNullOrEmpty(region) || string.IsNullOrEmpty(city) || string.IsNullOrEmpty(postalCode) || string.IsNullOrEmpty(streetAndNumber))
+            {
+                MessageBox.Show("Будь ласка, заповніть всі поля.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!postalCode.All(char.IsDigit) || postalCode.Length != 6)
+            {
+                MessageBox.Show("Невірний формат поштового коду. Введіть 6 цифр.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (streetAndNumber.Length <= 5)
+            {
+                MessageBox.Show("Введіть правильну вулицю та номер будинку.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string message = $"Область: {region}\nМісто: {city}\nПоштовий код: {postalCode}\nВулиця та номер будинку: {streetAndNumber}";
+            MessageBox.Show(message, "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string region = regionComboBox.SelectedItem?.ToString();
+            string city = cityComboBox.SelectedItem?.ToString();
+            string postalCode = postalCodeTextBox.Text;
+            string streetAndNumber = streetAndNumberTextBox.Text;
+
+            if (string.IsNullOrEmpty(region) || string.IsNullOrEmpty(city) || string.IsNullOrEmpty(postalCode) || string.IsNullOrEmpty(streetAndNumber))
+            {
+                MessageBox.Show("Будь ласка, заповніть всі поля.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!postalCode.All(char.IsDigit) || postalCode.Length != 6)
+            {
+                MessageBox.Show("Невірний формат поштового коду. Введіть 6 цифр.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (streetAndNumber.Length <= 5)
+            {
+                MessageBox.Show("Введіть правильну вулицю та номер будинку.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            string dataToWrite = $"Область: {region}\nМісто: {city}\nПоштовий код: {postalCode}\nВулиця та номер будинку: {streetAndNumber}";
+
+            string filePath = "result.txt";
+
+            try
+            {
+                System.IO.File.WriteAllText(filePath, dataToWrite);
+                MessageBox.Show("Інформацію збережено у файл result.txt", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при збереженні інформації у файл: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
